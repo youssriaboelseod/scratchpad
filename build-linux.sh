be29fffa721c6d673d747b87ffada1966a8ca8e7
LordImmaculate <emil.dhauwe@icloud.com>
2024-10-04 03:22:04 +0200

    Added build scripts

---
+++ build-linux.sh
@@ -0,0 +1,1 @@
+pyinstaller --onefile --noconsole --add-data "./style.css:." --add-data "./icons:icons" --icon="./icons/scratchpad.ico" scratchpad.py
---
+++ build.bat
@@ -0,0 +1,2 @@
+pyinstaller --onefile --noconsole --add-data ".\style.css;." --add-data ".\icons;icons" --icon=".\icons\scratchpad.ico" scratchpad.py
+pause > nul
