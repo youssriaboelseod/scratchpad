b7cabe9ce55a3940c12b3fa2c36b596bda3e430d
Raven <ravendevteam@proton.me>
2024-11-24 12:53:19 -0600

    Remove Windows-specific arguments from non-Windows build scripts

--- build-macos.sh
+++ build-macos.sh
@@ -1,1 +1,1 @@
-nuitka --onefile --standalone --enable-plugin=pyqt5 --remove-output --include-data-files=style.css=style.css --include-data-dir=icons=icons --windows-icon-from-ico=icons/scratchpad.ico --windows-console-mode=disable --output-dir=dist scratchpad.py
+nuitka --onefile --standalone --enable-plugin=pyqt5 --remove-output --include-data-files=style.css=style.css --include-data-dir=icons=icons --macos-app --output-dir=dist scratchpad.py
