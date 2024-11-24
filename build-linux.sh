bb29af47657b8c8db486e9c12c8958a71f462667
Raven <ravendevteam@proton.me>
2024-11-24 03:03:25 -0600

    Build executables using Nuitka instead of PyInstaller

--- build-linux.sh
+++ build-linux.sh
@@ -1,1 +1,1 @@
-pyinstaller --onefile --noconsole --add-data "./style.css:." --add-data "./icons:icons" --icon="./icons/scratchpad.ico" scratchpad.py
+nuitka --onefile --standalone --enable-plugin=pyqt5 --remove-output --include-data-files=style.css=style.css --include-data-dir=icons=icons --windows-icon-from-ico=icons/scratchpad.ico --windows-console-mode=disable --output-dir=dist scratchpad.py
--- build.bat
+++ build.bat
@@ -1,2 +1,3 @@
-pyinstaller --onefile --noconsole --add-data ".\style.css;." --add-data ".\icons;icons" --icon=".\icons\scratchpad.ico" scratchpad.py
-pause > nul
+@echo off
+nuitka --onefile --standalone --enable-plugin=pyqt5 --remove-output --include-data-files=style.css=style.css --include-data-dir=icons=icons --windows-icon-from-ico=icons/scratchpad.ico --windows-console-mode=disable --output-dir=dist scratchpad.py
+pause
