f2307d9e8c3f17aff738c11b1e4ffa89b3d53168
Raven <ravendevteam@proton.me>
2024-11-24 12:55:59 -0600

    Fix of bad argument in non-Windows build scripts

--- build-macos.sh
+++ build-macos.sh
@@ -1,1 +1,1 @@
-nuitka --onefile --standalone --enable-plugin=pyqt5 --remove-output --include-data-files=style.css=style.css --include-data-dir=icons=icons --macos-app --output-dir=dist scratchpad.py
+nuitka --onefile --standalone --enable-plugin=pyqt5 --remove-output --include-data-files=style.css=style.css --include-data-dir=icons=icons --no-console --output-dir=dist scratchpad.py
